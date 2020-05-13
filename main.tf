b88fc6adccfcd3b51a18f20d50599cf7ecef25bf
Akos Keresztes <Akos.Keresztes@sigmatechnology.se>
2020-05-13 15:50:02 +0200

    It does nothing yet

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 variable "hetzner_token" {}
 
 provider "hcloud" {
-	token = $var.hetzner_token
+	token = var.hetzner_token
 }
 
+data "hcloud_network" "nw_ahoy" {
+  name = "nw_default"
+}
+
+output "default_network_data" {
+  value = data.hcloud_network.nw_ahoy.ip_range
+}
